c8e11a4fb9c50abf1550d02b8730c2437c28ab95
Mohammad Yousry <mohammadyousry187@gmail.com>
2025-06-12 04:47:43 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,20 @@
-# firebase_config.py
-
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
-# ✅ تهيئة Firebase مرة واحدة فقط
+# ✅ تهيئة Firebase باستخدام متغير بيئي
 if not firebase_admin._apps:
-    cred = credentials.Certificate("/etc/secrets/firebase_key.json")  # غيّر للمسار الصحيح
+    firebase_key_json = os.environ.get("FIREBASE_KEY_JSON")
+    if not firebase_key_json:
+        raise ValueError("FIREBASE_KEY_JSON environment variable not set.")
+    
+    firebase_key_dict = json.loads(firebase_key_json)
+    cred = credentials.Certificate(firebase_key_dict)
+    
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'medi-go-eb65e.firebasestorage.app'
+        'storageBucket': 'medi-go-eb65e.appspot.com'
     })
 
-# ✅ العملاء الجاهزين
 db = firestore.client()
 bucket = storage.bucket()
